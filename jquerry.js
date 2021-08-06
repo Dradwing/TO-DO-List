53d464fa5b6407af80f002832039f56b32943cca
Dradwing <85051440+Dradwing@users.noreply.github.com>
2021-08-06 18:43:16 +0530

    Update jquerry.js

--- jquerry.js
+++ jquerry.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+    if(localStorage.getItem('list')!="")
+    {
+        $('ol').html(localStorage.getItem('list'));
+        $('.start').hide();
+    }
     var date=new Date();
-    document.querySelector('.date').innerText=date.toDateString();
+    document.querySelector('.date').innerText=date.toDateString();//jquery does not have date thing
     $('.refresh').click(function(){
         $('.list').empty();
+        localStorage.setItem('list',$('ol').html());
         $('.start').show();
     });
     $(document).keypress(function(e){
@@ -11,6 +17,7 @@ $(document).ready(function() {
             if($('input').val().trim()!="")
             {   $('.start').hide();
                 $('ol').append(`<li><i class="far fa-circle icons"></i><p>`+$('input').val()+`</p><i class="far fa-trash-alt deelete icons"></i></li>`);
+                localStorage.setItem('list',$('ol').html());
                 $('input').val('');
             }
             else{
@@ -22,14 +29,17 @@ $(document).ready(function() {
         $(this).removeClass();
         $(this).addClass("far fa-check-circle icons2");
         $(this).siblings("p").addClass("para"); 
+        localStorage.setItem('list',$('ol').html());
     });
     $(document).on('click','.icons2',function(){
         $(this).removeClass();
         $(this).addClass("far fa-circle icons");
         $(this).siblings("p").removeClass("para"); 
+        localStorage.setItem('list',$('ol').html());
     });
     $(document).on('click','.deelete',function(){
         $(this).parent().remove();
+        localStorage.setItem('list',$('ol').html());
         if($('ol').text().trim()==="")
         {
             $('.start').show();
